3999105345605869190eff2f2c6a335787a8ae02
pureworldnew <pureworldnew@gmail.com>
2020-10-05 12:18:48 -0700

    update content sending

--- app/Http/Controllers/TwilioController.php
+++ app/Http/Controllers/TwilioController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Twilio\Rest\Client;
+use Illuminate\Support\Facades\Log;
 
 class TwilioController extends Controller
 {
-    public function send(){
+    public function send(Request $request){
         $sid    = "ACbad198140433710a4e6a6bdfc880e1ec";
         $token  = "3127232a68128082edcc3d16f1b651f2";
         $twilio = new Client($sid, $token); 
@@ -16,7 +17,7 @@ class TwilioController extends Controller
                         ->create("whatsapp:+923055705456", // to
                                 [
                                     "from" => "whatsapp:+14155238886",
-                                    "body" => "Here is another test for twilio whatsapp "
+                                    "body" => $request->all()['items']
                                 ]
                         );
 
